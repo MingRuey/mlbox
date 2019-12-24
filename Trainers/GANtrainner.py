b21fb468efaa91f1a22c49f89ae94e51423d00a5
mrchou <imchou239@gmail.com>
2019-12-24 17:52:42 +0800

    fix few typos

--- Trainers/GANtrainner.py
+++ Trainers/GANtrainner.py
@@ -19,12 +19,12 @@ from tensorflow.keras.optimizers import SGD, Adam  # noqa: E402
 from tensorflow.keras.callbacks import TensorBoard, EarlyStopping  # noqa: E402
 from tensorflow.keras.callbacks import ReduceLROnPlateau  # noqa: E402
 
-from MLBOX.Datbase.dataset import DataBase  # noqa: E402
+from MLBOX.Database.dataset import DataBase  # noqa: E402
 from MLBOX.Scenes.SimpleSplit import SimpleSplit   # noqa: E402
 from MLBOX.Trainers.TF.Keras_Callbacks import ModelLogger, TrainRecord  # noqa: E402
 
 
-def disc_loss(true_pred: tf.Tensor, fake_pred: tf.Tensor):
+def disc_loss_fn(true_pred: tf.Tensor, fake_pred: tf.Tensor):
     """Loss function for discriminator
 
     Args:
@@ -32,16 +32,16 @@ def disc_loss(true_pred: tf.Tensor, fake_pred: tf.Tensor):
         fake_pred (tf.Tensor): the predictions of discriminator of fake images
     """
     true_lb = 0.9 * tf.ones_like(true_pred)
-    fake_lb = tf.zeors_like(fake_pred)
+    fake_lb = tf.zeros_like(fake_pred)
 
     lb = tf.stack([true_lb, fake_lb], axis=-1)
     pred = tf.stack([true_pred, fake_pred], axis=-1)
-    return keras.losses.BinaryCrossentropy(lb, pred)
+    return keras.losses.binary_crossentropy(lb, pred)
 
 
-def gen_loss(fake_pred: tf.Tensor):
+def gen_loss_fn(fake_pred: tf.Tensor):
     lb = tf.ones_like(fake_pred)
-    return keras.losses.BinaryCrossentropy(lb, fake_pred)
+    return keras.losses.binary_crossentropy(lb, fake_pred)
 
 
 class KerasGANTrainner:
@@ -68,11 +68,9 @@ class KerasGANTrainner:
         """
         self._gen = generator
         self._gen_optimzier = gen_optimizer
-        self._gen_loss = gen_loss
 
         self._disc = discriminator
         self._disc_optimzier = disc_optimizer
-        self._disc_loss = disc_loss
 
         if not pathlib.Path(str(out_dir)).is_dir():
             msg = "Invalid output dir, got {}"
@@ -84,12 +82,12 @@ class KerasGANTrainner:
 
     def train(
             self,
-            database: Database,
+            database: DataBase,
             batch_size: int = 8,
             max_epoch: int = 200,
             ):
 
-        dataset = database.get_datase(epoch=max_epoch, batch_size=batch_size)
+        dataset = database.get_dataset(epoch=max_epoch, batchsize=batch_size)
         steps_per_epoch = database.data_count // batch_size,
 
         epoch = 0
@@ -99,7 +97,7 @@ class KerasGANTrainner:
             print("epoch {} starts".format(epoch))
             for batch in steps_per_epoch:
                 batch += 1
-                gen_loss, disc_loss = _train_step(imgs, noises)
+                gen_loss, disc_loss = self._train_step(imgs, noises)
                 msg = "    batch {} - Generator loss: {}; Descriminator loss: {}"
                 print(msg.format(batch, gen_loss, disc_loss))
             print("epoch {} ends".format(epoch))
@@ -118,9 +116,13 @@ class KerasGANTrainner:
             real_out = self._disc(gt_imgs, training=True)
             fake_out = self._disc(fake_imgs, training=True)
 
-            gen_loss = gen_loss(fake_out)
-            disc_loss = disc_loss(real_out, fake_out)
+            gen_loss = gen_loss_fn(fake_out)
+            disc_loss = disc_loss_fn(real_out, fake_out)
 
-        self._gen_optimzier.minimize(gen_loss, self._gen.trainable_variables)
-        self._disc.minimize(gen_loss, self._disc.trainable_variables)
+        gen_grad = gen_tape.gradient(gen_loss, self._gen.trainable_variables)
+        disc_grad = disc_tape.gradient(disc_loss, self._disc.trainable_variables)
+
+        self._gen_optimzier.apply_gradients(zip(gen_grad, self._gen.trainable_variables))
+        self._disc_optimzier.apply_gradients(zip(disc_grad, self._disc.trainable_variables))
+
         return gen_loss, disc_loss
