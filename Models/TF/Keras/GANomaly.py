d3e089f05a2dee868c2e54650b3c44e1c73f706f
mrchou <imchou239@gmail.com>
2020-02-10 16:58:55 +0800

    first draft of GANomaly model

---
+++ Models/TF/Keras/GANomaly.py
@@ -0,0 +1,38 @@
+"""
+Implement following papers:
+
+GANomaly: Semi-Supervised Anomaly Detection via Adversarial Training
+"""
+from typing import List
+
+import tensorflow as tf
+import tensorflow.keras as keras
+
+from MLBOX.Models.TF.Keras.DCGAN import Generator, Discriminator
+from MLBOX.Models.TF.Keras.DCGAN import _batch_relu, _batch_leaky
+
+
+class GANomalyG:
+
+    def __init__(self, latent_size: int, image_shape: tuple):
+        self._shape = image_shape
+        self.decoder = Generator(image_shape)
+        self.encoder = Discriminator(n_out=latent_size)
+
+    def __call__(self, inputs: tf.Tensor) -> tf.Tensor:
+        # note that self.encoder always create new tensor on __call__
+        # so latent_i and latent_o have difference parametrizations
+        latent_i = self.encoder(inputs)
+        generated = self.decoder(latent_i)
+        latent_o = self.encoder(generated)
+        return generated, latent_i, latent_o
+
+
+class GANomalyD(Discriminator):
+
+    def __init__(self):
+        super().__init__(n_out=1)
+
+
+if __name__ == "__main__":
+    pass
